a258eca50d2cc7ab575732f963cdc8ac6e2994a3
veenhoff <patrick.veenhoff@apexdynamics.io>
2026-03-25 09:26:11 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BusinessCard from './business-card'
-
-ReactDOM.createRoot(document.getElementById('root')).render(<BusinessCard />)
