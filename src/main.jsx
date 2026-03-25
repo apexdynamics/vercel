a1e1dd6b812a7b83882c7095a0eb1db5e54fe649
veenhoff <patrick.veenhoff@apexdynamics.io>
2026-03-25 09:13:47 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BusinessCard from './business-card'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<BusinessCard />)
